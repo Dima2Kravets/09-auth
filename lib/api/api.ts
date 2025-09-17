225a007cd1f6fe53f582463ddac7dfc89abd7511
Dima_Kravets <dima2.kravets@gmail.com>
2025-09-18 00:54:31 +0300

    fix: Update baseURL to use NEXT_PUBLIC_API_URL instead of NEXT_PUBLIC_NEXTAUTH_URL

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosError } from 'axios';
 export type ApiError = AxiosError<{ error: string }>;
 
-const baseURL = `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api`;
-console.log("NEXT_PUBLIC_NEXTAUTH_URL:", process.env.NEXT_PUBLIC_NEXTAUTH_URL);
+const baseURL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
+console.log("NEXT_PUBLIC_NEXTAUTH_URL:", process.env.NEXT_PUBLIC_API_URL);
 console.log("baseURL:", baseURL);
 
 export const nextServer = axios.create({
